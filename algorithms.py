8b7b46470ca45f8727e8305b60950fae92c00400
Affan S <4442679+affans@users.noreply.github.com>
2026-02-02 16:29:40 -0600

    added base file with placeholders

---
+++ algorithms.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def z_score_normalize(data, threshold=3.0):
+    """Assigned to Aysha: Normalizes and removes outliers."""
+    # TODO: Implement logic
+    pass
+
+def impute_missing_values(data):
+    """Assigned to Priyanshu: Replaces None, "", and "NA" with the mean."""
+    # TODO: Implement logic
+    pass
+
+def min_max_scale(data):
+    """Assigned to Glenn: Scales values to the range ."""
+    # TODO: Implement logic
+    pass
