0842a33ee2f29a4ea6aff86caf8b41b144ed4165
aysha.sparrowhawk1@yahoo.ca <ass740@uregina.ca>
2026-02-04 19:54:43 -0600

    First Commit to feat-zscore branch

--- algorithms.py
+++ algorithms.py
@@ -5,30 +5,9 @@ fake_data = [2, 3, 4, 5, 6, 7, 1, 6, 90000]
 
 def z_score_normalize(data, threshold=3.0):
     """Assigned to Aysha: Normalizes and removes outliers."""
-    str_checker = False 
-    for i in range(0, len(data)):
-        if (isinstance(data[i], str) == True): #referenced geeksforgeeks
-            str_checker = True
-    if(len(data) == 0):
-        print("Dataset needs at least one data value. This is empty.\n")
-        clean_list = []
-    elif(str_checker == True):
-        print("List cannot contain any strings. This has a string in it.\n")
-        clean_list = [] #instead of returning the empty list, return the list without the string? 
-    else:
-        print("Correct format.\n")
-        mean_data = np.mean(data)
-        sd_data = np.std(data)
-        z_score = (data[0:len(data)] - mean_data)/sd_data
-        print("Before cleaning", z_score) #before we clean the list 
-        clean_list = [] #the list we are cleaning
-        for i in range(0, len(z_score)):
-            if z_score[i] < threshold:
-                #remove the values from the list 
-                clean_list.append(z_score[i])
-        print("after cleaning:", clean_list)
-    return (clean_list) #this is the cleaned list that we are returning
-    #pass
+    print("z-score normalizer function called")
+    # TODO: Implement logic
+    pass
 
 result = z_score_normalize(fake_data, threshold = 3.0)
 print(result)
